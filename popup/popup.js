7d002939287355067fbabf6eaa95516232b4b29a
Cody <cjosephp15@gmail.com>
2024-01-01 16:23:18 +1000

    Added logo that indicates blocking

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,28 @@
 const hideFundraiserBtn = document.getElementById("hide-fundraiser");
+const wikipediaLogo = document.getElementById("wikipedia-logo");
 let hideFundraiser = true;
 
+const changeWikipediaLogo = () => {
+  wikipediaLogo.src = hideFundraiser
+    ? "../images/blocking-enabled.png"
+    : "../images/blocking-disabled.png";
+};
+
 const changeFundraiserBtnText = () => {
-  if (hideFundraiser) {
-    hideFundraiserBtn.textContent = "Show Fundraiser";
-  } else {
-    hideFundraiserBtn.textContent = "Hide Fundraiser";
-  }
+  hideFundraiserBtn.textContent = hideFundraiser
+    ? "Show Fundraiser"
+    : "Hide Fundraiser";
 };
 
 chrome.storage.local.get("hideFundraiser", (res) => {
   hideFundraiser = res.hideFundraiser;
+  changeWikipediaLogo();
   changeFundraiserBtnText();
 });
 
 hideFundraiserBtn.addEventListener("click", () => {
   chrome.storage.local.set({ hideFundraiser: !hideFundraiser });
   hideFundraiser = !hideFundraiser;
+  changeWikipediaLogo();
   changeFundraiserBtnText();
 });
